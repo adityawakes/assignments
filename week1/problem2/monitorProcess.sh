a1c0b4b8f2245df4a2616a0bd15ef9a88c47d876
Aditya Purohit <aditya.purohit@inito.com>
2023-07-17 23:05:35 +0530

    Add timestamp along with pid as a unique identifier to search the process

--- week1/problem2/monitorProcess.sh
+++ week1/problem2/monitorProcess.sh
@@ -3,23 +3,20 @@
 zsh $1 &
 # Storing the process id
 myPID=$!
+timeStamp=$(ps axo pid,lstart | grep $myPID | awk '{print $2 $3 $4 $5 $6}')
 while true
 do
-# Randomly killing the process for testing code
-if [[ $(($RANDOM%1000)) -eq 0 ]]
-then
-echo "Killed process"
-kill -9 $myPID
-fi
 # Checking if the process is still running
 flag=$(ps axo pid | grep $myPID | wc -w)
-if [[ flag -eq 0 ]]
+curTime=$(ps axo pid,lstart | grep $myPID | awk '{print $2 $3 $4 $5 $6}')
+if [[ $flag -eq 0 || "$timeStamp" != "$curTime" ]]
 then
 # If the process is stopped, restarting it.
 echo "Process Stopped.Restarting..."
 sleep 1
 zsh $1 &
 myPID=$!
+timeStamp=$(ps axo pid,lstart | grep $myPID | awk '{print $2 $3 $4 $5 $6}')
 fi
 done
 
