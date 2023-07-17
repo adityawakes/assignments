f379b3c4bb4ebf2138fc22b4c44daa1c7a2f7033
Aditya Purohit <aditya.purohit@inito.com>
2023-07-17 14:53:55 +0530

    Add 2 files for output

--- week1/problem1/script.sh
+++ week1/problem1/script.sh
@@ -1,2 +1,5 @@
-echo "Count HTTPCode" > output.txt
-cat web-server.log |awk '{print $9}' | sort | uniq -c | sort -nr >> output.txt
+#! /bin/zsh
+echo "Count HTTPCode" > countAll.txt
+cat web-server.log |awk '{print $9}' | sort | uniq -c | sort -nr >> countAll.txt
+cat countAll.txt | awk '{if($2 == 200) print $1}' >> count200.txt
+
